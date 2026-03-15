db620c705f3212981547f9d678514a14a865141a
Rodrique <rodriquesep@gmail.com>
2026-03-15 14:22:30 +0200

    fixed netlify settings

--- purecore-erp/vite.config.js
+++ purecore-erp/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './' // ensures correct asset paths on Vercel
+  base: './', // ensures all assets work correctly
 })
